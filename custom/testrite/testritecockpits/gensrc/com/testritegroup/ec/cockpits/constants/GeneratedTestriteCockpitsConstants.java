15c88d15933ef1511d2a4fcfdb3f1c5ca336a52a
alex <dinglc@gmail.com>
2015-10-14 11:17:35 +0800

    no message

---
+++ custom/testrite/testritecockpits/gensrc/com/testritegroup/ec/cockpits/constants/GeneratedTestriteCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 17, 2015 3:47:46 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.testritegroup.ec.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTestriteCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "testritecockpits";
+	
+	protected GeneratedTestriteCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
